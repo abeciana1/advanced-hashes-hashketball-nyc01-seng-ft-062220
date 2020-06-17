e3e97c6f7a372eb55765c70c0fade0e2f7462d62
Alex Beciana <alex.beciana@gmail.com>
2020-06-17 02:09:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -179,8 +179,7 @@ def player_numbers(team_name)
   array = []
   
   game_hash.each do |(location, inner_hash)|
-    inner_hash.collect do |inner_key|
-      binding.pry
+    inner_hash[:players].each do |inner_key|
       if inner_hash[:team_name] == team_name
         array.push(inner_key[:number])
       end
