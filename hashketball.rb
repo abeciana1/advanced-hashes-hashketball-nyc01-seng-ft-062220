37837d370754466ba2297a8e72313f22f0ba7926
Alex Beciana <alex.beciana@gmail.com>
2020-06-17 03:01:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,7 +194,7 @@ def player_stats(player_name)
   game_hash.each do |(location, inner_hash)|
     inner_hash[:players].each do |inner_key|
       if inner_key[:player_name] == player_name
-        binding.pry
+        return inner_key
       end
     end
   end
