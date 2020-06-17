02e914df3a51ec73fd62718110c02b785fd48f1d
Alex Beciana <alex.beciana@gmail.com>
2020-06-17 02:49:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -185,7 +185,6 @@ def player_numbers(name_of_team)
     inner_hash[:players].collect do |inner_key|
       if inner_hash[:team_name] == name_of_team
         final_array.push(inner_key[:number])
-        binding.pry
       end
       return final_array
     end
